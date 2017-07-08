947254853f61a038fe360773982f737e7c20e3f3
LAPTOP-NB6TOFGE\yuki <010110yuki@gmail.com>
2017-07-08 23:06:52 +0900

    UIの表示を微調整しました。

--- 剣戟乱舞α/剣戟乱舞α/UI.cpp
+++ 剣戟乱舞α/剣戟乱舞α/UI.cpp
@@ -5,17 +5,26 @@
 #include"Sub.h"
 #include"UI.h"
 
+#define UI_MAX 5
+
+static float ux[UI_MAX] = { 0, 50, 100, 150, 200 };
+static float uy[UI_MAX] = { 0, 0, 0, 0, 0 };
 
 void UIdraw()
 {
-	CUSTOMVERTEX life[] = {
-		{0.f ,0.f , 1.f, 1.f, 0xFFFFFFFF, 0.f, 0.f },
-		{100.f,0.f , 1.f, 1.f, 0xFFFFFFFF, 1.f, 0.f },
-		{100.f,100.f, 1.f, 1.f, 0xFFFFFFFF, 1.f, 1.f },
-		{0.f ,100.f, 1.f, 1.f, 0xFFFFFFFF, 0.f, 1.f }
-	};
-	g_pD3Device->SetTexture(0, g_pTexture[life_TEX]);
-	g_pD3Device->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, life, sizeof(CUSTOMVERTEX));
+	for (int i = 0; i < 5; i++)
+	{
+		CUSTOMVERTEX life[] = 
+		{
+			{ 50.f + ux[i],  0.f + uy[i], 1.f, 1.f, 0xFFFFFFFF, 1.f, 0.f },
+			{ 50.f + ux[i], 50.f + uy[i], 1.f, 1.f, 0xFFFFFFFF, 1.f, 1.f },
+			{  0.f + ux[i], 50.f + uy[i], 1.f, 1.f, 0xFFFFFFFF, 0.f, 1.f },
+			{  0.f + ux[i],  0.f + uy[i], 1.f, 1.f, 0xFFFFFFFF, 0.f, 0.f }
+		};
+
+		g_pD3Device->SetTexture(0, g_pTexture[life_TEX]);
+		g_pD3Device->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, life, sizeof(CUSTOMVERTEX));
+	}
 }
 
 
